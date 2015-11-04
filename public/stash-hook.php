a51a2b121c34a881b54b7937d36b89448ce91651
Samo Gabrovec <samo.gabrovec@pop-tv.si>
2015-11-04 15:50:55 +0100

    Reload dashboard every 20 sec

--- public/stash-hook.php
+++ public/stash-hook.php
@@ -1,22 +1,43 @@
 <?php
 
+/**
+ * Stash pushes commit info to this intermediate file.
+ *
+ * This file parses commit info and calls PHPCI webhook to
+ * start the build process.
+ */
 
+/*
+ * This is what stash sends us
+ * See https://confluence.atlassian.com/bitbucketserver/post-service-webhook-for-bitbucket-server-776640367.html
+ */
 $commit = json_decode(file_get_contents('php://input'), 1);
+$refId = $commit['refChanges'][0]['refId'];
+
+// if this is not a commit into branch we're not interested in
+// building
+if (strpos($refId, 'refs/heads/') === false) {
+    return;
+}
 
 $qstring = [];
 
-$qstring['branch'] = str_replace('refs/heads/', '', $commit['refChanges'][0]['refId']);
+$qstring['branch'] = str_replace('refs/heads/', '', $refId);
 $qstring['newrev'] = $commit['changesets']['values'][0]['toCommit']['id'];
 $qstring['committer'] = $commit['changesets']['values'][0]['toCommit']['author']['emailAddress'];
 $qstring['message'] = $commit['changesets']['values'][0]['toCommit']['message'];
 
 $projectId = $commit['repository']['slug'];
 
-$url = "http://" . $_SERVER['HTTP_HOST'] . "/webhook/git/$projectId?"
+/**
+ * Url of the phpci testing server
+ */
+$url = "http://" . $_SERVER['HTTP_HOST']
+        . '/webhook/git/' . $projectId . '?'
         . http_build_query($qstring);
 
 echo "OK";
 
 $ret = file_get_contents($url);
 
-error_log("\n".$url."\n".$ret."\n", 3, "/tmp/stash.log");
+//error_log("\n".$url."\n".$ret."\n", 3, "/tmp/stash.log");
